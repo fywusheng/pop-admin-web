d9aa04d23ce4e0837273dd080f3fe0b297e46e88
wanghongtao <8701246+wanghongtao0713@user.noreply.gitee.com>
2022-03-23 23:14:45 +0800

    更新

--- src/views/commodity-module/product/list.vue
+++ src/views/commodity-module/product/list.vue
@@ -3,7 +3,7 @@
     <el-tabs v-model="activeType" type="card" @tab-click="handleClick" style="padding-top:0">
       <el-tab-pane label="销售中" name="1" :disabled="disabled"></el-tab-pane>
       <el-tab-pane label="已下架" name="2" :disabled="disabled"></el-tab-pane>
-      <el-tab-pane label="商品库" name="3" :disabled="disabled"></el-tab-pane>
+      <el-tab-pane label="商品池" name="3" :disabled="disabled"></el-tab-pane>
     </el-tabs>
     <el-form :inline="true">
     <el-row class="mb-2">
@@ -51,6 +51,18 @@
               :props="{value:'id',label:'name',leaf:'parentCode',children: 'children',expandTrigger: 'hover',emitPath:false}"
             ></el-cascader>
         </el-form-item>
+        <el-form-item label="" v-if="activeType==4">
+          <el-select
+            v-model="searchParams.auditState"
+            size="mini"
+            clearable
+            placeholder="请选择审核状态...">
+            <el-option value="1" label="草稿中"></el-option>
+            <el-option value="2" label="待审核"></el-option>
+            <el-option value="3" label="审核不通过"></el-option>
+            <el-option value="4" label="审核通过"></el-option>
+          </el-select>
+        </el-form-item>
     </el-row>
     <el-row class="mb-2" style="height:38px">
         <el-form-item style="background:#F2F6FC">
@@ -58,8 +70,8 @@
           <el-button v-if="activeType==1" type="default" icon="el-icon-bottom" size="mini" @click="stopedSelling()">下 架</el-button>
           <el-button v-if="activeType==2" type="default" icon="el-icon-top" size="mini" @click="publishedSelling()">上 架</el-button>
           <!-- <el-button v-if="activeType==3" type="default" size="small" @click="forward2ProductAddPage">审 批</el-button> -->
-          <el-button v-if="activeType==4" type="default" icon="el-icon-circle-plus-outline" size="mini" @click="forward2ProductAddPage()">新 增</el-button>
-          <el-button v-if="activeType==4" type="default" size="mini" @click="exportProduct" icon="el-icon-download">导 出</el-button>
+          <el-button v-if="activeType==3" type="default" icon="el-icon-circle-plus-outline" size="mini" @click="forward2ProductAddPage()">新 增</el-button>
+          <el-button v-if="activeType==3" type="default" size="mini" @click="exportProduct" icon="el-icon-download">导 出</el-button>
           <!-- <el-button v-if="activeType==4" type="default" size="small" @click="forward2ProductAddPage" icon="el-icon-upload">导 入</el-button> -->
         </el-form-item>
     </el-row>
@@ -86,19 +98,31 @@
       <el-table-column prop="name" label="商品名称" show-overflow-tooltip></el-table-column>
       <el-table-column prop="id" label="商品ID" width="150px" show-overflow-tooltip></el-table-column>
       <el-table-column prop="code" label="商品编码" width="150px" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="brandName" label="所属品牌" width="150px" show-overflow-tooltip></el-table-column>
+      <el-table-column prop="brandId" label="所属品牌" width="150px" show-overflow-tooltip></el-table-column>
       <el-table-column prop="categoryNode" label="所属类目" width="150px" show-overflow-tooltip></el-table-column>
       <el-table-column prop="saleState" label="销售状态" width="90px" align="center" :formatter="formatSaleState"></el-table-column>
       <el-table-column prop="saleOnlineTime" label="上架时间" width="150px" v-if="activeType==1" align="center"></el-table-column>
       <el-table-column prop="saleStopTime" label="下架时间" width="150px" v-if="activeType==2" align="center"></el-table-column>
-      <el-table-column prop="auditState" label="审核状态" width="100px" v-if="activeType==3" align="center" :formatter="formatAuditState"></el-table-column>
-      <el-table-column prop="updatedTime" label="修改时间" width="150px" v-if="activeType==3" align="center"></el-table-column>
+      <el-table-column prop="auditState" label="审核状态" width="100px" v-if="activeType==4" align="center" :formatter="formatAuditState"></el-table-column>
+      <el-table-column prop="auditTimeTime" label="审核时间" width="100px" v-if="activeType==3" align="center"></el-table-column>
+      <el-table-column prop="updatedTime" label="修改时间" width="150px" v-if="activeType==4" align="center"></el-table-column>
+      <!--el-table-column prop="delFlag" label="状态" width="80px" v-if="activeType==4">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.delFlag1"
+            active-color="#67C23A"
+            inactive-color="#909399"
+          >
+          </el-switch>
+        </template>
+      </el-table-column-->
       <el-table-column prop="" label="操作" align="center" width="280px">
         <template slot-scope="scope">
-          <el-button size="mini" v-if="activeType==3" @click="forward2SpecPage(scope.row)">SKU管理</el-button>
-          <el-button size="mini" v-if="activeType==2||activeType==3" icon="el-icon-edit" @click="forward2ProductAddPage(scope.row)">编辑</el-button>
+          <el-button size="mini" v-if="activeType==4" @click="forward2SpecPage(scope.row)">SKU管理</el-button>
+          <el-button size="mini" v-if="activeType==2||activeType==4" icon="el-icon-edit" @click="forward2ProductAddPage(scope.row)">编辑</el-button>
           <el-button size="mini" v-if="activeType==1" icon="el-icon-bottom" @click="stopSingleSelection(scope.row)">下架</el-button>
           <el-button size="mini" v-if="activeType==2" icon="el-icon-top" @click="publishSingleSelection(scope.row)">上架</el-button>
+          <el-button size="mini" v-if="activeType==4" :disabled="scope.row.auditState!=2" icon="el-icon-success" @click="audit(scope.row)">审核</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -128,11 +152,11 @@ export default {
       totalCount: 20,
       loading: false,
       disabled: false,
-      activeType:'3',
+      activeType:'4',
       brandOptions:[],
       categoryOptions:[],
       searchParams: {
-        queryType:3,
+        queryType:4,
         name:'',
         id:'',
         code:'',
@@ -150,10 +174,10 @@ export default {
   },
   methods: {
     formatSaleState: function (row, column) {
-        return row.saleState === 10 ? '在售中' : row.saleState === 11 ? '上架中' : row.saleState === 20 ? '已停售' : row.saleState === 21 ? '下架中' : '--'
+        return row.saleState === 5 ? '在售中' : row.saleState === 51 ? '上架中' : row.saleState === 6 ? '已停售' : row.saleState === 61 ? '下架中' : '--'
     },
     formatAuditState: function (row, column) {
-        return row.auditState === -1 ? '草稿中' : row.auditState === 0 ? '待审核' : row.auditState === -2 ? '审核不通过' : row.auditState === 1 ? '审核通过' : '--'
+        return row.auditState === 2 ? '待审核' : row.auditState === 3 ? '审核不通过' : row.auditState === 4 ? '审核通过' : '--'
     },
     handleClick(tab, event) {
       this.dataList = []
@@ -166,8 +190,7 @@ export default {
       } else if (this.activeType === '3') {
         this.searchParams.queryType = 3
       } else {
-        this.$message.warning('不支持该查询类型！')
-        return
+        this.searchParams.queryType = 4
       }
       this.loadData();
     },
@@ -204,7 +227,6 @@ export default {
       const params = {
         pageNum: this.pageNo,
         pageSize: this.pageSize,
-        queryType:'product-list',
         queryObject:this.searchParams
       }
       const result = await post('/product/listByPageNo', params)
@@ -292,22 +314,61 @@ export default {
       .catch(() => {});
     },
 
+    audit(row){
+      var auditState = 4
+      this.$confirm("该商品资料完整度是否满足销售规则，符合则通过否则请驳回?", "审核确认提示", {
+        distinguishCancelAndClose: true,
+        confirmButtonText: "通过",
+        cancelButtonText: "驳回",
+        type: "warning",
+      })
+      .then(async () => {
+          this.loading = true
+          const result = await post('/product/audit', {id:row.id,auditState:4})
+          this.loading = false
+          if (result.code == 200) {
+            this.$message.success("商品审核成功！")
+              this.loadData()
+          } 
+          else {
+            this.$message.error(result.msg)
+          }
+      })
+      .catch(async action => {
+        if(action==='cancel'){
+          this.loading = true
+          const result = await post('/product/audit', {id:row.id,auditState:3})
+          this.loading = false
+          if (result.code == 200) {
+            this.$message.success("商品审核成功！")
+              this.loadData()
+          } 
+          else {
+            this.$message.error(result.msg)
+          }
+        }
+      });
+    },
+
+    forward2ProductAddPage(row) {
+      if(row){
+        this.$router.push({ name: 'ProductAddPage', params: { id: row.id } })
+      }
+      else{
+        this.$router.push({ name: 'ProductAddPage', params: { id: -1} })
+      }
+    },
     forward2SpecPage(row) {
       this.$router.push({ name: 'SkuListDetails', params: { categoryNode:row.categoryNode,id: row.id } })
     },
 
     async exportProduct(){
-      if(!this.multipleSelection||this.multipleSelection.length==0){
-        this.$message.error('请选择需要导出的商品！')
-        return 
-      }
-      const params = {
-        // pageNum: this.pageNo,
-        // pageSize: this.pageSize,
-        // queryObject:this.searchParams
-        list: this.multipleSelection
-      }
       this.loading = true
+      const params = {
+        pageNum: this.pageNo,
+        pageSize: this.pageSize,
+        queryObject:this.searchParams
+      }
       await downloadByPost('/product/export','商品列表.xlsx', params)
       this.loading = false
     }
